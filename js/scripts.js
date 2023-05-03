b61c1ceb46099d4722187cef4259e90b8f323e9d
Colby Crawford <colbycrawford2@gmail.com>
2023-05-03 07:27:31 -0700

    Added Initial Pokemon Object Items

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert('Hello World');
+let pokemonList = []
 
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+let pokemon1 = {
+    name: 'Bulbasaur',
+    height: .7,
+    type: ['grass', 'poison']
+}
+
+let pokemon2 = {
+    name: 'Squirtle',
+    height: .5,
+    type: ['water']
+}
+
+let pokemon3 = {
+    name: 'Gyarados',
+    height: 6.5,
+    type: ['water', 'flying']
+}
+
+let pokemon4 = {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying']
+}
