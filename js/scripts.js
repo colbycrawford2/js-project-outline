4eb3a658fc0fb0f370de3dc259f767e35e4d8801
Colby Crawford <colbycrawford2@gmail.com>
2023-05-15 19:42:24 -0700

    Added Pokemon array & for Loop
    Added pokemon objects to pokemonList array. Also cerated for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
-let pokemonList = []
+let pokemonList = [
 
-let pokemon1 = {
+{
     name: 'Bulbasaur',
     height: .7,
     type: ['grass', 'poison']
-}
+},
 
-let pokemon2 = {
+{
     name: 'Squirtle',
     height: .5,
     type: ['water']
-}
+},
 
-let pokemon3 = {
+{
     name: 'Gyarados',
     height: 6.5,
     type: ['water', 'flying']
-}
+},
 
-let pokemon4 = {
+{
     name: 'Charizard',
     height: 1.7,
     type: ['fire', 'flying']
+}
+];
+
+for (let i = 0; i < pokemonList.length; i++ ) {
+   if (pokemonList[i].name = 'Bulbasaur '){
+   document.write(pokemonList[i].name + '(height: .7)');}
+
 }
