eec7e9f765ad6f83dcd2843f7cbcea6cd9903e8b
Colby Crawford <colbycrawford2@gmail.com>
2023-05-18 06:20:43 -0700

    Corrected typos and Edited text
    line 29 changed == to >=, line 30 changed text, line 32 corrected / to . ,

--- js/scripts.js
+++ js/scripts.js
@@ -26,9 +26,9 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++ ) {
-   if (pokemonList[i].height == '1.0'){
-   document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ') - wow that\'s a big<br/>');
+   if (pokemonList[i].height >= 1.0){
+   document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ') - wow, that\'s big<br/>');
 } else {
-    document.write(pokemonList[i].name + '(height: ' + pokemonList[i]/height + ')<br/>')
+    document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')<br/>')
 }
 }
