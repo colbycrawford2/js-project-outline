6c9f54c7e8db677716f939539b31957309ad393c
Colby Crawford <colbycrawford2@gmail.com>
2023-05-18 06:30:50 -0700

    Added comment for for loop

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,7 @@ let pokemonList = [
 }
 ];
 
+//for loop shows large pokemon//
 for (let i = 0; i < pokemonList.length; i++ ) {
    if (pokemonList[i].height >= 1.0){
    document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ') - wow, that\'s big<br/>');
