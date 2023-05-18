86958acf6b5975d73d769df032c24d2ed9eb77b8
Colby Crawford <colbycrawford2@gmail.com>
2023-05-18 06:35:58 -0700

    Edited indentations in for loop

--- js/scripts.js
+++ js/scripts.js
@@ -28,8 +28,9 @@ let pokemonList = [
 //for loop shows large pokemon//
 for (let i = 0; i < pokemonList.length; i++ ) {
    if (pokemonList[i].height >= 1.0){
-   document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ') - wow, that\'s big<br/>');
+       document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ') - wow, that\'s big<br/>');
 } else {
-    document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')<br/>')
-}
+       document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')<br/>')
+       }
+       
 }
