91a7923e4b97b0d83d1fed8f9f06130debec0a0b
Colby Crawford <colbycrawford2@gmail.com>
2023-07-01 11:12:56 -0700

    Created IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,45 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: .7,
+            type: ['grass', 'poison']
+        },         
+        {
+            name: 'Squirtle',
+            height: .5,
+            type: ['water']
+        },
 
-{
-    name: 'Bulbasaur',
-    height: .7,
-    type: ['grass', 'poison']
-},
+        {
+            name: 'Gyarados',
+            height: 6.5,
+            type: ['water', 'flying']
+        },
 
-{
-    name: 'Squirtle',
-    height: .5,
-    type: ['water']
-},
+        {
+            name: 'Charizard',
+            height: 1.7,
+            type: ['fire', 'flying']
+        }
+    ]
 
-{
-    name: 'Gyarados',
-    height: 6.5,
-    type: ['water', 'flying']
-},
+    function getAll() {
+        return pokemonList;
+    }
 
-{
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-}
-];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+   
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})();
+
+console.log(pokemonRepository.getAll())
 
 //for loop shows large pokemon//
 /*for (let i = 0; i < pokemonList.length; i++ ) {
@@ -32,11 +48,11 @@ let pokemonList = [
 } else {
        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<br/>')
        }
-
 }*/
+let pokemonList = pokemonRepository.getAll();
 
 pokemonList.forEach(function(pokemon){
-    console.log(pokemon.name + 'is' + pokemon.height + 'is' + pokemon.type + 'is');
+    console.log(pokemon.name + pokemon.height + pokemon.type);
 });
 
 function divide(dividend, divisor){
