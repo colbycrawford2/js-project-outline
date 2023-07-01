c30c779586a48629a03e010d32f60939da61e710
Colby Crawford <colbycrawford2@gmail.com>
2023-07-01 10:04:20 -0700

    Created forEach() loop

--- js/scripts.js
+++ js/scripts.js
@@ -26,11 +26,28 @@ let pokemonList = [
 ];
 
 //for loop shows large pokemon//
-for (let i = 0; i < pokemonList.length; i++ ) {
+/*for (let i = 0; i < pokemonList.length; i++ ) {
    if (pokemonList[i].height >= 1.0){
        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - wow, that\'s big<br/>');
 } else {
        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<br/>')
        }
 
-}
+}*/
+
+pokemonList.forEach(function(pokemon){
+    console.log(pokemon.name + 'is' + pokemon.height + 'is' + pokemon.type + 'is');
+});
+
+function divide(dividend, divisor){
+    if(divisor === 0){
+        return "You're trying to divide by 0."
+    }else{
+        let result = dividend / divisor;
+        return result;
+        }    
+    }
+    console.log(divide(4, 2));
+    console.log(divide(7, 0));
+    console.log(divide(1, 4));
+    console.log(divide(12, -3));
