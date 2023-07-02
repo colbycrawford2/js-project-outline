a854e1c019533ed3c9f86323f790b7f73acbbeb0
Colby Crawford <colbycrawford2@gmail.com>
2023-07-02 13:20:55 -0700

    Removed pokemon objects array
    replaced with empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,5 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: 'Bulbasaur',
-            height: .7,
-            type: ['grass', 'poison']
-        },         
-        {
-            name: 'Squirtle',
-            height: .5,
-            type: ['water']
-        },
-
-        {
-            name: 'Gyarados',
-            height: 6.5,
-            type: ['water', 'flying']
-        },
-
-        {
-            name: 'Charizard',
-            height: 1.7,
-            type: ['fire', 'flying']
-        }
-    ]
+    let pokemonList = []
 
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
