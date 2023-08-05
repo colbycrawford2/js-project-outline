ea6fc349548e4deb3d25e19f3790a67749277d56
Colby Crawford <colbycrawford2@gmail.com>
2023-08-05 10:15:27 -0700

    Added code for Modal
    Added modal code to edit for exercise 1.8

--- js/scripts.js
+++ js/scripts.js
@@ -87,6 +87,79 @@ let pokemonRepository = (function () {
     });
 });
 
+(function() {
+    let modalContainer = document.querySelector('#modal-container');
+    let dialogPromiseReject; // This can be set later, by showDialog
+    
+    function showModal(title, text) {
+      // Clear all existing modal content
+      modalContainer.innerHTML = '';
+      
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+      
+      // Add the new modal content
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+      
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = title;
+      
+      let contentElement = document.createElement('p');
+      contentElement.innerText = text;
+      
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modalContainer.appendChild(modal);
+      
+      modalContainer.classList.add('is-visible');
+    }
+    
+    function hideModal() {
+      modalContainer.classList.remove('is-visible');
+      
+      if (dialogPromiseReject) {
+        dialogPromiseReject();
+        dialogPromiseRejct = null;
+      }
+    }
+    
+    function showDialog(title, text) {
+      showModal(title, text);
+      
+      // We want to add a confirm and cancel button to the modal
+      let modal = modalContainer.querySelector('.modal');
+      
+      let confirmButton = document.createElement('button');
+      confirmButton.classList.add('modal-confirm');
+      confirmButton.innerText = 'Confirm';
+      
+      let cancelButton = document.createElement('button');
+      cancelButton.classList.add('modal-cancel');
+      cancelButton.innerText = 'Cancel';
+      
+      modal.appendChild(confirmButton);
+      modal.appendChild(cancelButton);
+      
+      // We want to focus the confirmButton so that the user can simply press Enter
+      confirmButton.focus();
+      
+      // Return a promise that resolves when confirmed, else rejects
+      return new Promise((resolve, reject) => {
+        cancelButton.addEventListener('click', hideModal);
+        confirmButton.addEventListener('click', () => {
+          dialogPromiseReject = null; // Reset this
+          hideModal();
+          resolve();
+        });
+        // This can be used to reject from other functions
+        dialogPromiseReject = reject;
+      });
+    }
+
 //for loop shows large pokemon//
 //can I delete the for loop?//
 /*for (let i = 0; i < pokemonList.length; i++ ) {
